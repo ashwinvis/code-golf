19342d51f14683c7406f17c9c8d78e313abbf09e
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-04-18 00:03:23 +0200

    Python: Fibonacci: 144 chars with lambdas

--- python/fibonacci.py
+++ python/fibonacci.py
@@ -1,5 +1,2 @@
-def fib(n):
-    return int(bool(n)) if n < 3 else fib(n-1) + fib(n-2)
-
-for i in range(31):
-    print(fib(i))
+fib = lambda n: int(bool(n)) if n < 3 else fib(n-1) + fib(n-2)
+for i in range(31): print(fib(i))
