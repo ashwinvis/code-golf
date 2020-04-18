71428c29ca6eba0f199ea482022d80c20e6c3b2d
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-04-18 19:03:28 +0200

    Minify pi solution to 237 chars

--- python/pi.py
+++ python/pi.py
@@ -1,14 +1,15 @@
 # https://stackoverflow.com/questions/4484489/using-basic-arithmetics-for-calculating-pi-with-arbitary-precision
-from decimal import getcontext, Decimal
+from decimal import *
 
 
 getcontext().prec = 1001
 pi = 0
+D = Decimal
 for k in range(1001):
-    pi += (Decimal(1) / (16 ** k)) * (
-        (Decimal(4) / (8 * k + 1))
-        - (Decimal(2) / (8 * k + 4))
-        - (Decimal(1) / (8 * k + 5))
-        - (Decimal(1) / (8 * k + 6))
+    pi += (D(1) / (16 ** k)) * (
+        (D(4) / (8 * k + 1))
+        - (D(2) / (8 * k + 4))
+        - (D(1) / (8 * k + 5))
+        - (D(1) / (8 * k + 6))
     )
 print(pi)
